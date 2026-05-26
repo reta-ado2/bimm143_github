b124dafe15bf89fd31351f48ab5387c5960d9806
Reta Ado <rado@ucsd.edu>
2026-05-26 12:18:02 -0700

    Added Class04 and Class11

---
+++ Class04/Lab 04 Text File .R
@@ -0,0 +1,15 @@
+#' ---
+#' title: "Class4: Intro to R"
+#' author: "Reta Ado"
+#' date: "April 9, 2026"
+#' ---
+
+
+# This is just a text file 
+x <- 1:50 
+plot(x, sin(x), type = "l", col = "blue", lwd = 3) 
+
+
+
+
+
